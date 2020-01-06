6317a7b4cb431be05e238e020b8f7e8c2b01c70a
amit.fegade121@gmail.com <amit.fegade121@gmail.com>
2020-01-06 12:12:14 +0530

    Added amazon.books.json file

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import PostForm from "./components/PostForm";
 import AllPosts from "./components/AllPosts";
+import Test from "./components/Test";
 
 function App() {
   return (
     <div className="container">
       <PostForm />
       <AllPosts />
+      {/* <Test /> */}
     </div>
   );
 }
